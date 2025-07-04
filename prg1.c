d933b04fcdf3278c40d5afd9452c6eb727f2bda0
pushkarpurs <pushkar.p.urs@gmail.com>
2025-07-05 02:32:27 +0530

    Exploring the Superblock and Block Group Descriptor Table

--- prg1.c
+++ prg1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define BLOCK_SIZE 1024
+#include <math.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -14,35 +14,92 @@ int main(int argc, char *argv[]) {
         perror("Failed to open image file");
         return 1;
     }
+	
+	struct SuperBlock{
+		uint32_t blocksize;
+		uint32_t totblocks;
+		uint32_t totinodes;
+		uint32_t inodesgrp;
+		uint32_t blocskgrp;
+		uint32_t grps;
+		uint16_t fstate;
+	}sb;
+	
+	//SuperBlock sb;
 
-    unsigned char buffer[BLOCK_SIZE];
+    unsigned char buffer[1024];
     int block_num = 1;
 	
-	if (fseek(img, block_num * BLOCK_SIZE, SEEK_SET) != 0) {
+	if (fseek(img, block_num * 1024, SEEK_SET) != 0) {
         perror("Failed to seek to block");
         return 1;
     }
 
-    if (fread(buffer, 1, BLOCK_SIZE, img) == BLOCK_SIZE) {
+    if (fread(buffer, 1, 1024, img) == 1024) {
 		
         // printf("Block %d:\n", block_num++);
-        // for (int i = 0; i < BLOCK_SIZE; i++) {
+        // for (int i = 0; i < 1024; i++) {
             // printf("%02x ", buffer[i]);
             // if ((i + 1) % 16 == 0) printf("\n");
         // }
 		if(buffer[56]==83 && buffer[57]==239)
-			printf("The image is in Ext2 format");
+			printf("The image is in Ext2 format\n");
 		else{
 			fprintf(stderr, "Image is nhot in Ext2 format");
 			return 1;
 		}
+		uint32_t* fours=(uint32_t* )buffer;
+		uint16_t* twos=(uint16_t* )buffer; 
+		sb.fstate=twos[29];
+		if(twos[29]==1)
+		{
+			printf("File system state is clean\n");
+		}
+		else{
+			printf("File system state isnt clean\n");
+		}
+		if(ceil(fours[1]*1.0/fours[8])==ceil(fours[0]*1.0/fours[10])){
+			sb.grps=(int)ceil(fours[1]*1.0/fours[8]);
+			sb.totblocks=fours[1];
+			sb.totinodes=fours[0];
+			sb.inodesgrp=fours[10];
+			sb.blocskgrp=fours[8];
+			printf("Number of blocks groups= %d\n", (int)ceil(fours[1]*1.0/fours[8]));
+			printf("Number of blocks per group= %d\n", fours[8]);
+			printf("Number of inodes per group= %d\n", fours[10]);
+		}
+		else{
+			printf("Mismatch number of block groups");
+		}
+		sb.blocksize=1024<<fours[6];
+		printf("Block size=%d\n",1024<<fours[6]);
 		}
 	else{
         perror("Error while reading");
         fclose(img);
         return 1;
 		}
+		
+	unsigned char buffer2[sb.blocksize];
 
+	int seek=sb.blocksize;
+	if (seek==1024)
+		seek=2048;
+
+	fseek(img, seek, SEEK_SET);
+
+	if (fread(buffer2, 1, sb.blocksize, img) == sb.blocksize)
+	{
+		uint32_t* fours2=(uint32_t* )buffer2;
+		uint16_t* twos2=(uint16_t* )buffer2; 
+		printf("Block address of block usage bitmap %d\n", fours2[0]);
+		printf("Block address of inode usage bitmap %d\n", fours2[1]);
+		printf("Starting block address of inode table %d\n", fours2[2]);
+		printf("Number of unallocated blocks in group %d\n", twos2[6]);
+		printf("Number of unallocated inodes in group %d\n", twos2[7]);
+		printf("Number directories in group %d\n", twos2[8]);
+	}
+	
     fclose(img);
     return 0;
 }
